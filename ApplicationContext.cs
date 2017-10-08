7d9617d2892973bbd5192c90410d7549848b64cc
lipatkin96 <lipatkin96@gmail.com>
2017-10-08 20:49:47 +0700

    Start

---
+++ ApplicationContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+	
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreConsoleApp1
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=exampledb;
+                            Username=lipatkin;Password=qwerty;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // using Fluent API
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<User>(entity =>
+        //    {
+        //        entity.ToTable("Users", "public");
+
+        //        entity.Property(e => e.Id)
+        //                            .HasColumnName("id")
+        //                            .HasDefaultValueSql("nextval('public.users_id_seq'::regclass)");
+
+        //        entity.Property(e => e.Age).HasColumnName("age");
+
+        //        entity.Property(e => e.Name)
+        //                            .IsRequired()
+        //                            .HasColumnName("name");
+        //    });
+
+        //    modelBuilder.HasSequence("users_id_seq", "public");
+        //}
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (var db = new ApplicationContext())
+                {
+
+                    // Creating a new item and saving it to the database
+                    var newUser = new User();
+                    newUser.Id = 1;
+                    newUser.Name = "Red Apple";
+                    newUser.Age = 12;
+                    db.Users.Add(newUser);
+                    var count = db.SaveChanges();
+                    Console.WriteLine("{0} records saved to database", count);
+
+                    //// Retrieving and displaying data
+                    //Console.WriteLine();
+                    //Console.WriteLine("All items in the database:");
+                    //foreach (var user in db.Users)
+                    //{
+                    //    Console.WriteLine("{0} | {1}", user.Name, user.Age);
+                    //}
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+            }
+        }
+    }
+}
